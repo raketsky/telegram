7d348247cc722aec778051988c2539a68458779a
Burt0n <ncom3000@gmail.com>
2018-10-14 21:59:10 +0300

    Updates

--- src/Telegram/Client.php
+++ src/Telegram/Client.php
@@ -5,11 +5,46 @@ use Raketsky\Component\TelegramSendMessage;
 
 class Client
 {
+	private $apiUrl = 'https://api.telegram.org/bot';
+	
 	use TelegramSendMessage;
 	
 	public function __construct($token, $adminChatId = null)
 	{
 		$this->setTelegramSendMessageToken($token);
 		$this->setTelegramSendMessageAdmin($adminChatId);
+	}
+	
+	public function getUpdates($count=3)
+	{
+		$data = $this->getUpdatesRaw();
+		$data = array_reverse($data);
+		
+		$items = [];
+		foreach ($data as $i => $r) {
+			if ($i >= $count) {
+				break;
+			}
+			$items[] = [
+				'id' => $r->message->message_id,
+				'name' => $r->message->from->first_name,
+				'username' => $r->message->from->username,
+				'text' => isset($r->message->text) ? $r->message->text : null,
+				'chat_id' => $r->message->chat->id,
+				'created_at' => date('Y-m-d H:i:s', $r->message->date),
+			];
+		}
+		$items = array_reverse($items);
+		
+		return $items;
+	}
+	
+	public function getUpdatesRaw()
+	{
+		$url = $this->apiUrl.$this->token.'/getUpdates';
+		$data = file_get_contents($url);
+		$data = json_decode($data);
+		
+		return $data->result;
 	}
 }
